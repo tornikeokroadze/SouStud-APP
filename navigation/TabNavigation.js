2e1692640a26ef32881f08ffa93e2e6deb17b53a
IurkaAntia <yuraantia@gmail.com>
2024-10-10 21:14:08 +0400

    Add authentication files

---
+++ navigation/TabNavigation.js
@@ -0,0 +1,97 @@
+import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import SubjectScreen from "../screens/SubjectScreen";
+import JobScreen from "../screens/JobScreen";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { icons } from "../constants";
+import { Image, StyleSheet, Dimensions } from "react-native";
+
+const { width } = Dimensions.get("window");
+const Tab = createMaterialBottomTabNavigator();
+export default function TabNavigation() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#FFFFFF"
+      inactiveColor="#FFFFFF"
+      barStyle={{ backgroundColor: "#673ab7" }}
+      shifting={true}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.home}
+              resizeMode="contain"
+              style={[
+                styles.icon,
+                { tintColor: focused ? "#673ab7" : "#FFFFFF" },
+              ]}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Subject"
+        component={SubjectScreen}
+        options={{
+          tabBarLabel: "Subject",
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.book}
+              resizeMode="contain"
+              style={[
+                styles.icon,
+                { tintColor: focused ? "#673ab7" : "#FFFFFF" },
+              ]}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Job"
+        component={JobScreen}
+        options={{
+          tabBarLabel: "Job",
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.job}
+              resizeMode="contain"
+              style={[
+                styles.icon,
+                { tintColor: focused ? "#673ab7" : "#FFFFFF" },
+              ]}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.user}
+              resizeMode="contain"
+              style={[
+                styles.icon,
+                { tintColor: focused ? "#673ab7" : "#FFFFFF" },
+              ]}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: width * 0.06,
+    height: width * 0.06,
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,70 +1,88 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList, Platform, SafeAreaView } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Platform,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
+
+const { width } = Dimensions.get("window");
 
 const subjects = [
-  { id: '1', name: 'Programilebis sawyisebi', score: 80 },
-  { id: '2', name: 'Algoritmebis sawyisebi', score: 90 },
-  { id: '3', name: 'Linux', score: 65 },
+  { id: "1", name: "Programilebis sawyisebi", score: 80 },
+  { id: "2", name: "Algoritmebis sawyisebi", score: 90 },
+  { id: "3", name: "Linux", score: 65 },
 ];
 
 const assignments = [
-  { id: '1', title: 'Sabakalavro Dacva', dueDate: '2024-10-15' },
-  { id: '2', title: 'Shualeduri Exam', dueDate: '2024-10-22' },
+  { id: "1", title: "Sabakalavro Dacva", dueDate: "2024-10-15" },
+  { id: "2", title: "Shualeduri Exam", dueDate: "2024-10-22" },
 ];
 
 const notifications = [
-  { id: '1', message: 'Finaluris qula daido benidzen chagwrat!', date: '2024-10-05' },
-  { id: '2', message: 'New assignment posted for Algoritmebis sawyisebi.', date: '2024-10-06' },
+  {
+    id: "1",
+    message: "Finaluris qula daido benidzen chagwrat!",
+    date: "2024-10-05",
+  },
+  {
+    id: "2",
+    message: "New assignment posted for Algoritmebis sawyisebi.",
+    date: "2024-10-06",
+  },
 ];
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#f8f9fa' }}>
-    <View style={styles.container}>
-      <Text style={styles.header}>Welcome to StudApp</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f8f9fa" }}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Welcome to StudApp</Text>
 
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Subjects</Text>
-        <FlatList
-          data={subjects}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text style={styles.itemText}>{item.name}</Text>
-              <Text style={styles.scoreText}>Score: {item.score}</Text>
-            </View>
-          )}
-        />
-      </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Subjects</Text>
+          <FlatList
+            data={subjects}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                <Text style={styles.itemText}>{item.name}</Text>
+                <Text style={styles.scoreText}>Score: {item.score}</Text>
+              </View>
+            )}
+          />
+        </View>
 
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>mosalodneli gamocdeni an rame</Text>
-        <FlatList
-          data={assignments}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text style={styles.itemText}>{item.title}</Text>
-              <Text style={styles.dueDateText}>Due: {item.dueDate}</Text>
-            </View>
-          )}
-        />
-      </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>mosalodneli gamocdeni an rame</Text>
+          <FlatList
+            data={assignments}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                <Text style={styles.itemText}>{item.title}</Text>
+                <Text style={styles.dueDateText}>Due: {item.dueDate}</Text>
+              </View>
+            )}
+          />
+        </View>
 
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Notifications</Text>
-        <FlatList
-          data={notifications}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text style={styles.notificationText}>{item.message}</Text>
-              <Text style={styles.dateText}>{item.date}</Text>
-            </View>
-          )}
-        />
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Notifications</Text>
+          <FlatList
+            data={notifications}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                <Text style={styles.notificationText}>{item.message}</Text>
+                <Text style={styles.dateText}>{item.date}</Text>
+              </View>
+            )}
+          />
+        </View>
       </View>
-    </View>
     </SafeAreaView>
   );
 };
@@ -73,53 +91,53 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f8f9fa',
-    marginTop: Platform.OS === 'android' && width * 0.02
+    backgroundColor: "#f8f9fa",
+    marginTop: Platform.OS === "android" && width * 0.02,
   },
   header: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    color: '#673ab7',
-    textAlign: 'center',
+    color: "#673ab7",
+    textAlign: "center",
   },
   section: {
     marginBottom: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 8,
     padding: 15,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 10,
     elevation: 5,
   },
   sectionTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   item: {
     paddingVertical: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
   },
   itemText: {
     fontSize: 16,
   },
   scoreText: {
     fontSize: 14,
-    color: '#555',
+    color: "#555",
   },
   dueDateText: {
     fontSize: 14,
-    color: '#888',
+    color: "#888",
   },
   notificationText: {
     fontSize: 16,
   },
   dateText: {
     fontSize: 14,
-    color: '#888',
+    color: "#888",
   },
 });
 
